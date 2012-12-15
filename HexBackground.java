ec18d629241c0f5d2500aea290a4e1479778fac8
cchandurkar <cchandurkar@gmail.com>
2012-12-15 06:19:29 -0800

    HexaDecimal.java added to repo

---
+++ HexBackground.java
@@ -0,0 +1,36 @@
+package ch.games.snatch.jack4.utils;
+
+import org.andengine.entity.scene.background.Background;
+
+import android.util.Log;
+
+public class HexBackground extends Background{
+	public HexBackground(String hex){	
+		if(hex.length()>6){
+			Log.e("HexBackground", "Hex must be 6 character long.");
+			Log.e("HexBackground", "Example:");
+			Log.e("HexBackground", "setBackground(new HexBackground(\"028482\"));");
+			setColor(1,1,1);
+		}else{
+			setColor(getRed(hex), getGreen(hex), getBlue(hex));
+		}
+	}
+
+	private static float getGreen(String hex) {
+		float mGreen 	= Integer.parseInt(hex.charAt(2)+""+hex.charAt(3),16)/255f;
+		Log.d("HexBackground", "Green: "+mGreen);
+		return mGreen;
+	}
+
+	private static float getRed(String hex) {
+		float mRed 	= Integer.parseInt((hex.charAt(0)+""+hex.charAt(1)),16)/255f;
+		Log.d("HexBackground", "Red: "+Integer.parseInt(hex.charAt(2)+""+hex.charAt(3),16)/255f);
+		return mRed;
+	}
+
+	private static float getBlue(String hex) {
+		float mBlue	 = Integer.parseInt(hex.charAt(4)+""+hex.charAt(5),16)/255f;
+		Log.d("HexBackground", "Blue : "+mBlue);
+		return mBlue;
+	}
+}
